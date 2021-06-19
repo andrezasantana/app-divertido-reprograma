1d6fa2dcd0c0914b7cd89a7276269255c66ffddd
Andreza Santana <andreza.santana16@gmail.com>
2021-06-19 17:05:20 -0300

    cria pagina simpsons

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 import Home from '../pages/home'
+import Simpsons from '../pages/simpsons'
 
 const Routes = () => {
     return(
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route path='/simpsons' component={Simpsons} />
             </Switch>
         </BrowserRouter>
     )
